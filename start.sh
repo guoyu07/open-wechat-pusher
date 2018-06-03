b2e5e39e49e908ab18052cd959bd7fc76f99cd0b
朱海峰 <zhuhaifeng@parkingwang.com>
2018-06-03 14:06:58 +0800

    fix start.shell

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ echo "build program....."
 go build -o wechat-pusher . 
 
 echo "start new process ..."
-nohup ./wechat-pusher -c config/queues.yml -log logs/wechat-pusher -pidfile run/wechat-pusher &
+nohup ./wechat-pusher -c config/queues.yml -log logs/wechat-pusher.pid -pidfile run/wechat-pusher &
 
 exit 0
